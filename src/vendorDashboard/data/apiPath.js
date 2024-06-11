35716d184a767337eb242e309ec2043fbf6c0e69
srinu8688 <srinu868890@gmail.com>
2024-06-11 15:54:08 +0530

    changed path to live in api url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,2 +1,2 @@
-//export const API_URL="https://backend-nodejs-swiggy-clone.onrender.com"
-export const API_URL="http://localhost:4000"
+export const API_URL="https://backend-nodejs-swiggy-clone.onrender.com"
+//export const API_URL="http://localhost:4000"
